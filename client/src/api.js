2be74ea7d14925985fd18107a8d9420470a96c22
Pachu03 <franfervaz03@gmail.com>
2024-12-02 09:19:01 +0100

    creacion de js respaldo

---
+++ client/src/api.js
@@ -0,0 +1,9 @@
+export const api_sources = "http://localhost:3000/sources";
+export const url_nba = "https://nba-latest-news.p.rapidapi.com/articles";
+export const options_nba = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "46a1195708mshabbc7c245bb1465p1beed6jsn90f6065ff70c",
+    "x-rapidapi-host": "nba-latest-news.p.rapidapi.com",
+  },
+};
